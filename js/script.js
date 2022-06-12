7c8f4c9dbdc17611c0f95c71d965861064a09a43
Pendalf763 <107370740+Pendalf763@users.noreply.github.com>
2022-06-13 01:06:39 +0400

    new site

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function Init(){
+    if($.cookie("theme") == "light"){
+        $('html').attr('theme','light');
+    }
+    if($.cookie("theme") == 'dark'){
+        $('html').attr('theme','dark');
+    }
+    if($.cookie("theme") == 'dark'){
+        $('.logo').attr('src','img/logo-light.svg');
+    }
+    if($.cookie("theme") == 'dark'){
+        $('#themeIcon').attr('src','img/sun.svg');
+    }
+    if(!$.cookie("theme")){
+        $('html').attr('theme','light');
+    }
+}
+
+
+$(document).ready(function(){
+    Init();
+    $('#theme').click(function(){ 
+        if($('html').attr('theme') == 'light'){
+            $('html').attr('theme','dark');
+            $('.logo').attr('src', 'img/logo-light.svg');
+            $('#themeIcon').attr('src','img/sun.svg');
+            $.cookie("theme",'dark');
+        }
+        else{
+            $('html').attr('theme','light');
+            $('.logo').attr('src', 'img/logo-dark.svg');
+            $('#themeIcon').attr('src','img/moon.svg');
+            $.cookie("theme",'light');
+        }
+    })
+
+    $('.faq__item').click(function(){
+        if($('.faq__right').hasClass('one')){
+            $('.faq__item').not($(this)).removeClass('active')
+            $('.faq__overview').not($(this).children('.faq__overview')).slideUp(300)
+        }
+        $(this).toggleClass('active').children('.faq__overview').slideToggle(300)
+    })
+
+ });
+
+
+feather.replace();
